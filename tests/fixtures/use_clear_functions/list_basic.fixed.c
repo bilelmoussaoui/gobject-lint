f93bfc65c5be036b293f0aadb95cf0feb0d58123
Gaël Bonithon <gael@xfce.org>
2026-07-04 17:53:14 +0200

    use_clear_functions: Add support for g_s?list_free_full
    Fixes: #140

--- tests/fixtures/use_clear_functions/list_basic.fixed.c
+++ tests/fixtures/use_clear_functions/list_basic.fixed.c
@@ -5,5 +5,17 @@ my_func (GList *list, GSList *slist)
 {
   g_clear_list (&list, NULL);
 
+  g_clear_list (&list, g_free);
+
   g_clear_slist (&slist, NULL);
+
+  g_clear_slist (&slist, g_free);
+
+  g_clear_list (&list, NULL);
+
+  g_clear_list (&list, g_free);
+
+  g_clear_slist (&slist, NULL);
+
+  g_clear_slist (&slist, g_free);
 }
