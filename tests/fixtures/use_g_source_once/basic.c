aaac47d9567742f66c056e6b104af2c4f353c879
Gaël Bonithon <gael@xfce.org>
2026-07-12 12:44:59 +0200

    use_g_source_once: Properly match g_(idle|timeout)_add calls
    This rule could be further refined by determining whether other uses of
    the GSourceFunc ignore its return value (which is likely often the
    case), and replacing it with a GSourceOnceFunc in those cases as well.
    Two false negatives reported in #137 are of this type, but this seems
    more like an improvement than a bug fix.
    
    Fixes: #137

--- tests/fixtures/use_g_source_once/basic.c
+++ tests/fixtures/use_g_source_once/basic.c
@@ -7,8 +7,26 @@ my_idle_cb (gpointer user_data)
   return G_SOURCE_REMOVE;
 }
 
+static gboolean
+my_timeout_cb (gpointer user_data)
+{
+  do_work ();
+  return G_SOURCE_REMOVE;
+}
+
+static gboolean
+my_timeout_seconds_cb (gpointer user_data)
+{
+  do_work ();
+  return G_SOURCE_REMOVE;
+}
+
 static void
 setup (void)
 {
   g_idle_add (my_idle_cb, NULL);
+
+  guint id = g_timeout_add (10, my_timeout_cb, NULL);
+
+  gpointer pid = GUINT_TO_POINTER (g_timeout_add_seconds (1, my_timeout_seconds_cb, NULL));
 }
--- tests/fixtures/use_g_source_once/basic.fixed.c
+++ tests/fixtures/use_g_source_once/basic.fixed.c
@@ -7,7 +7,23 @@ my_idle_cb (gpointer user_data)
 }
 
 static void
+my_timeout_cb (gpointer user_data)
+{
+  do_work ();
+}
+
+static void
+my_timeout_seconds_cb (gpointer user_data)
+{
+  do_work ();
+}
+
+static void
 setup (void)
 {
   g_idle_add_once (my_idle_cb, NULL);
+
+  guint id = g_timeout_add_once (10, my_timeout_cb, NULL);
+
+  gpointer pid = GUINT_TO_POINTER (g_timeout_add_seconds_once (1, my_timeout_seconds_cb, NULL));
 }
